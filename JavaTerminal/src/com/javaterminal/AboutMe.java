c7be840c41934c81539442892d1432d9e8d77302
Priscila Almeida <priscilaalmeida.dev@gmail.com>
2024-08-21 20:20:21 -0300

    adicionando exceptions

--- JavaTerminal/src/com/javaterminal/AboutMe.java
+++ JavaTerminal/src/com/javaterminal/AboutMe.java
@@ -1,12 +1,13 @@
 package com.javaterminal;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
 
 	public static void main(String[] args) {
-		
+		try {
 		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 		
 		System.out.println("Digite seu nome: ");
@@ -25,6 +26,9 @@ public class AboutMe {
 		System.out.println("Tenho " + idade + " anos");
 		System.out.println("Minha altura é " + altura + "cm");
 
+		}catch(InputMismatchException e) {
+			System.out.println("Os campos idade e altura precisam ser numéricos");
+		}
 	}
 
 }
---
+++ JavaTerminal/src/com/javaterminal/CepInvalidoException.java
@@ -0,0 +1,5 @@
+package com.javaterminal;
+
+public class CepInvalidoException extends Exception{
+
+}
