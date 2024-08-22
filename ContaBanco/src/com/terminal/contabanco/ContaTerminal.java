da4f41c69ef019a816c97dac2b55b25da6975958
Priscila Almeida <priscilaalmeida.dev@gmail.com>
2024-08-22 15:26:27 -0300

    alterando funcionalidades

--- ContaBanco/src/com/terminal/contabanco/ContaTerminal.java
+++ ContaBanco/src/com/terminal/contabanco/ContaTerminal.java
@@ -9,17 +9,23 @@ public class ContaTerminal {
 		
 		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 		
-		int conta = 8437;
-		double saldo = 3500;
-		
 		System.out.println("Olá, seja bem vindo ao Banco Novo Horizonte!");
 		System.out.println("Por favor, digite seu nome: ");
 		String nome  = scanner.next();
 		
 		System.out.println("Por favor, digite o número da Agência! ");
-		int agencia  = scanner.nextInt();
+		String agencia  = scanner.next();
+		
+		// Formata o valor inserido para o formato "000-0" de maneira compacta
+        String agenciaFormatada = String.format("%03d-%d", Integer.parseInt(agencia) / 10, Integer.parseInt(agencia) % 10);
+		
+		System.out.println("Por favor, digite o número da Conta! ");
+		int numero = scanner.nextInt();
+		
+		System.out.println("Quanto deseja depositar em sua conta? ");
+		double saldo = scanner.nextDouble();
 		
 		System.out.println("Olá, " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " 
-		+ agencia + ", sua conta é " + conta + " e seu saldo R$" + saldo + " já está disponível para saque.");
+		+ agenciaFormatada + ", sua conta é " + numero + " e seu saldo R$" + String.format("%.2f", saldo) + " já está disponível para saque.");
 	}
 }
